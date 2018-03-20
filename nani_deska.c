0003480d1d8606b896a9dd3632f2982a023ba753
Falconarhia <kta.11@mail.ru>
2018-03-20 23:07:49 +0700

    Realize output desk state

---
+++ nani_deska.c
@@ -0,0 +1,16 @@
+#include "nani_deska.h"
+
+void show_deska (char deska[8][8])
+{
+	for(int i=0; i< 8; ++i )
+	{
+		printf("%d ", (8-i));
+		for(int j=0; j<8; ++j)
+		{
+			printf("%c ", deska[i][j]);
+		} 
+		printf("\n");
+	}
+
+printf("a b c d e f g h\n");
+}
---
+++ nani_deska.h
@@ -0,0 +1,7 @@
+#ifndef NANI_DESKA
+#define NANI_DESKA
+#include <stdio.h>
+
+void show_deska(int deska[8][8]);
+
+#endif
